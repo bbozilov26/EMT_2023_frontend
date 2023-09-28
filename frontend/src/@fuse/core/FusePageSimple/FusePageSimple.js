65481618d39c081bd9a646f2615867c0b2af5a76
Bojan Bozilov <bobobozilov@gmail.com>
2023-09-28 13:24:38 +0200

    show user's balance; realign content

--- frontend/src/@fuse/core/FusePageSimple/FusePageSimple.js
+++ frontend/src/@fuse/core/FusePageSimple/FusePageSimple.js
@@ -1,109 +1,109 @@
-import FuseScrollbars from '@fuse/core/FuseScrollbars';
-import { styled } from '@mui/material/styles';
-import clsx from 'clsx';
-import * as PropTypes from 'prop-types';
-import { forwardRef, memo, useImperativeHandle, useRef } from 'react';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import FusePageSimpleHeader from './FusePageSimpleHeader';
-import FusePageSimpleSidebar from './FusePageSimpleSidebar';
+import FuseScrollbars from "@fuse/core/FuseScrollbars";
+import { styled } from "@mui/material/styles";
+import clsx from "clsx";
+import * as PropTypes from "prop-types";
+import { forwardRef, memo, useImperativeHandle, useRef } from "react";
+import GlobalStyles from "@mui/material/GlobalStyles";
+import FusePageSimpleHeader from "./FusePageSimpleHeader";
+import FusePageSimpleSidebar from "./FusePageSimpleSidebar";
 
-const Root = styled('div')(({ theme, ...props }) => ({
-  display: 'flex',
-  flexDirection: 'column',
+const Root = styled("div")(({ theme, ...props }) => ({
+  display: "flex",
+  flexDirection: "column",
   minWidth: 0,
-  minHeight: '100%',
-  position: 'relative',
-  flex: '1 1 auto',
-  width: '100%',
-  height: 'auto',
+  minHeight: "100%",
+  position: "relative",
+  flex: "1 1 auto",
+  width: "100%",
+  height: "auto",
   backgroundColor: theme.palette.background.default,
 
-  '&.FusePageSimple-scroll-content': {
-    height: '100%',
+  "&.FusePageSimple-scroll-content": {
+    height: "100%",
   },
 
-  '& .FusePageSimple-wrapper': {
-    display: 'flex',
-    flexDirection: 'row',
-    flex: '1 1 auto',
+  "& .FusePageSimple-wrapper": {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 1 auto",
     zIndex: 2,
     minWidth: 0,
-    height: '100%',
+    height: "100%",
     backgroundColor: theme.palette.background.default,
 
-    ...(props.scroll === 'content' && {
-      position: 'absolute',
+    ...(props.scroll === "content" && {
+      position: "absolute",
       top: 0,
       bottom: 0,
       right: 0,
       left: 0,
-      overflow: 'hidden',
+      overflow: "hidden",
     }),
   },
 
-  '& .FusePageSimple-header': {
-    display: 'flex',
-    flex: '0 0 auto',
-    backgroundSize: 'cover',
+  "& .FusePageSimple-header": {
+    display: "flex",
+    flex: "0 0 auto",
+    backgroundSize: "cover",
   },
 
-  '& .FusePageSimple-topBg': {
-    position: 'absolute',
+  "& .FusePageSimple-topBg": {
+    position: "absolute",
     left: 0,
     right: 0,
     top: 0,
     height: headerHeight,
-    pointerEvents: 'none',
+    pointerEvents: "none",
   },
 
-  '& .FusePageSimple-contentWrapper': {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '100%',
-    flex: '1 1 auto',
-    overflow: 'hidden',
+  "& .FusePageSimple-contentWrapper": {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    flex: "1 1 auto",
+    overflow: "hidden",
     //    WebkitOverflowScrolling: 'touch',
     zIndex: 9999,
   },
 
-  '& .FusePageSimple-toolbar': {
+  "& .FusePageSimple-toolbar": {
     height: toolbarHeight,
     minHeight: toolbarHeight,
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
   },
 
-  '& .FusePageSimple-content': {
-    display: 'flex',
-    flex: '1 1 auto',
-    alignItems: 'start',
+  "& .FusePageSimple-content": {
+    display: "flex",
+    flex: "1 1 auto",
+    alignItems: "start",
     minHeight: 0,
-    overflowY: 'auto',
+    overflowY: "auto",
   },
 
-  '& .FusePageSimple-sidebarWrapper': {
-    overflow: 'hidden',
-    backgroundColor: 'transparent',
-    position: 'absolute',
-    '&.permanent': {
-      [theme.breakpoints.up('lg')]: {
-        position: 'relative',
+  "& .FusePageSimple-sidebarWrapper": {
+    overflow: "hidden",
+    backgroundColor: "transparent",
+    position: "absolute",
+    "&.permanent": {
+      [theme.breakpoints.up("lg")]: {
+        position: "relative",
         marginLeft: 0,
         marginRight: 0,
-        transition: theme.transitions.create('margin', {
+        transition: theme.transitions.create("margin", {
           easing: theme.transitions.easing.sharp,
           duration: theme.transitions.duration.leavingScreen,
         }),
-        '&.closed': {
-          transition: theme.transitions.create('margin', {
+        "&.closed": {
+          transition: theme.transitions.create("margin", {
             easing: theme.transitions.easing.easeOut,
             duration: theme.transitions.duration.enteringScreen,
           }),
 
-          '&.FusePageSimple-leftSidebar': {
+          "&.FusePageSimple-leftSidebar": {
             marginLeft: -props.leftsidebarwidth,
           },
-          '&.FusePageSimple-rightSidebar': {
+          "&.FusePageSimple-rightSidebar": {
             marginRight: -props.rightsidebarwidth,
           },
         },
@@ -111,60 +111,60 @@ const Root = styled('div')(({ theme, ...props }) => ({
     },
   },
 
-  '& .FusePageSimple-sidebar': {
-    position: 'absolute',
+  "& .FusePageSimple-sidebar": {
+    position: "absolute",
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.primary,
 
-    '&.permanent': {
-      [theme.breakpoints.up('lg')]: {
-        position: 'relative',
+    "&.permanent": {
+      [theme.breakpoints.up("lg")]: {
+        position: "relative",
       },
     },
-    maxWidth: '100%',
-    height: '100%',
+    maxWidth: "100%",
+    height: "100%",
   },
 
-  '& .FusePageSimple-leftSidebar': {
+  "& .FusePageSimple-leftSidebar": {
     width: props.leftsidebarwidth,
 
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up("lg")]: {
       borderRight: `1px solid ${theme.palette.divider}`,
       borderLeft: 0,
     },
   },
 
-  '& .FusePageSimple-rightSidebar': {
+  "& .FusePageSimple-rightSidebar": {
     width: props.rightsidebarwidth,
 
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up("lg")]: {
       borderLeft: `1px solid ${theme.palette.divider}`,
       borderRight: 0,
     },
   },
 
-  '& .FusePageSimple-sidebarHeader': {
+  "& .FusePageSimple-sidebarHeader": {
     height: headerHeight,
     minHeight: headerHeight,
     backgroundColor: theme.palette.primary.dark,
     color: theme.palette.primary.contrastText,
   },
 
-  '& .FusePageSimple-sidebarHeaderInnerSidebar': {
-    backgroundColor: 'transparent',
-    color: 'inherit',
-    height: 'auto',
-    minHeight: 'auto',
+  "& .FusePageSimple-sidebarHeaderInnerSidebar": {
+    backgroundColor: "transparent",
+    color: "inherit",
+    height: "auto",
+    minHeight: "auto",
   },
 
-  '& .FusePageSimple-sidebarContent': {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100%',
+  "& .FusePageSimple-sidebarContent": {
+    display: "flex",
+    flexDirection: "column",
+    minHeight: "100%",
   },
 
-  '& .FusePageSimple-backdrop': {
-    position: 'absolute',
+  "& .FusePageSimple-backdrop": {
+    position: "absolute",
   },
 }));
 
@@ -191,21 +191,21 @@ const FusePageSimple = forwardRef((props, ref) => {
     <>
       <GlobalStyles
         styles={(theme) => ({
-          ...(props.scroll !== 'page' && {
-            '#fuse-toolbar': {
-              position: 'static',
+          ...(props.scroll !== "page" && {
+            "#fuse-toolbar": {
+              position: "static",
             },
-            '#fuse-footer': {
-              position: 'static',
+            "#fuse-footer": {
+              position: "static",
             },
           }),
-          ...(props.scroll === 'page' && {
-            '#fuse-toolbar': {
-              position: 'sticky',
+          ...(props.scroll === "page" && {
+            "#fuse-toolbar": {
+              position: "sticky",
               top: 0,
             },
-            '#fuse-footer': {
-              position: 'sticky',
+            "#fuse-footer": {
+              position: "sticky",
               bottom: 0,
             },
           }),
@@ -213,7 +213,7 @@ const FusePageSimple = forwardRef((props, ref) => {
       />
       <Root
         className={clsx(
-          'FusePageSimple-root',
+          "FusePageSimple-root",
           `FusePageSimple-scroll-${props.scroll}`,
           props.className
         )}
@@ -228,7 +228,7 @@ const FusePageSimple = forwardRef((props, ref) => {
               <FusePageSimpleSidebar
                 position="left"
                 content={props.leftSidebarContent}
-                variant={props.leftSidebarVariant || 'permanent'}
+                variant={props.leftSidebarVariant || "permanent"}
                 ref={leftSidebarRef}
                 rootRef={rootRef}
                 open={props.leftSidebarOpen}
@@ -243,8 +243,8 @@ const FusePageSimple = forwardRef((props, ref) => {
               {props.header && <FusePageSimpleHeader header={props.header} />}
               {props.content && (
                 <FuseScrollbars
-                  enable={props.scroll === 'content'}
-                  className={clsx('FusePageSimple-content container')}
+                  enable={props.scroll === "content"}
+                  className={clsx("container")}
                 >
                   {props.content}
                 </FuseScrollbars>
@@ -255,7 +255,7 @@ const FusePageSimple = forwardRef((props, ref) => {
               <FusePageSimpleSidebar
                 position="right"
                 content={props.rightSidebarContent}
-                variant={props.rightSidebarVariant || 'permanent'}
+                variant={props.rightSidebarVariant || "permanent"}
                 ref={rightSidebarRef}
                 rootRef={rootRef}
                 open={props.rightSidebarOpen}
@@ -276,7 +276,7 @@ FusePageSimple.propTypes = {
   rightSidebarVariant: PropTypes.node,
   header: PropTypes.node,
   content: PropTypes.node,
-  scroll: PropTypes.oneOf(['normal', 'page', 'content']),
+  scroll: PropTypes.oneOf(["normal", "page", "content"]),
   leftSidebarOpen: PropTypes.bool,
   rightSidebarOpen: PropTypes.bool,
   leftSidebarWidth: PropTypes.number,
@@ -287,7 +287,7 @@ FusePageSimple.propTypes = {
 
 FusePageSimple.defaultProps = {
   classes: {},
-  scroll: 'page',
+  scroll: "page",
   leftSidebarOpen: false,
   rightSidebarOpen: false,
   rightSidebarWidth: 240,
--- frontend/src/app/main/apps/e-commerce/daily-check-ins/DailyCheckInsTable.js
+++ frontend/src/app/main/apps/e-commerce/daily-check-ins/DailyCheckInsTable.js
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 
 import { useDispatch, useSelector } from "react-redux";
 import withRouter from "@fuse/core/withRouter";
@@ -12,6 +12,9 @@ import {
 } from "../store/dailyCheckInsSlice";
 import DailyCheckInRepository from "../repositories/DailyCheckInRepository";
 import UserRepository from "../repositories/UserRepository";
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import FuseSvgIcon from "@fuse/core/FuseSvgIcon";
 
 function DailyCheckInsTable(props) {
   const dispatch = useDispatch();
@@ -70,21 +73,39 @@ function DailyCheckInsTable(props) {
   }
 
   return (
-    <div className="flex flex-col flex-1 w-full mx-auto px-24 pt-24 sm:p-40">
-      <motion.div
-        className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-8 lg:gap-16 mt-32 sm:mt-40"
-        variants={container}
-        initial="hidden"
-        animate="show"
-      >
-        {userDailyCheckIns.map((udci) => {
-          return (
-            <motion.div variants={item} key={udci.id}>
-              <DailyCheckInCard udci={udci} user={user} />
-            </motion.div>
-          );
-        })}
-      </motion.div>
+    <div className="flex items-center justify-between">
+      <div className="text-18 flex items-center ml-64">
+        Your Balance:
+        <FuseSvgIcon className="mr-2">
+          heroicons-outline:currency-euro
+        </FuseSvgIcon>
+        {user.creditBalance ?? 0}
+      </div>
+      <div className="mr-64">
+        <Card>
+          <CardContent>
+            <div className="flex flex-col flex-1 w-full mx-auto px-16 pt-8 sm:p-16">
+              <motion.div
+                className="flex grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4 md:gap-8 lg:gap-16 mt-2 sm:mt-4"
+                variants={container}
+                initial="hidden"
+                animate="show"
+                style={{
+                  justifyItems: "center", // Center items horizontally
+                }}
+              >
+                {userDailyCheckIns.map((udci) => {
+                  return (
+                    <motion.div variants={item} key={udci.id}>
+                      <DailyCheckInCard udci={udci} user={user} />
+                    </motion.div>
+                  );
+                })}
+              </motion.div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
